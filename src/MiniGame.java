adf86a55ddc54f9d691c7e5a37f6d992524dc112
Aman Jain <aman.jain@gatech.edu>
2019-02-08 10:33:38 -0500

    Testing Git

--- src/MiniGame.java
+++ src/MiniGame.java
@@ -1,5 +1,7 @@
 public class MiniGame extends GamePiece<Player> implements Game<Player>{
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Helloooooo");
     }
+
 }
