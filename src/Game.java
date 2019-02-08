3ec54bf78307d902a90941c660f3f6c5261c5cf1
Aman Jain <aman.jain@gatech.edu>
2019-02-08 10:42:11 -0500

    Deleted some errors. Let's see if this works

--- src/Game.java
+++ src/Game.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public interface Game<T> {
 
-    GamePiece<T>[][] board = new GamePiece[3][3];
+    GamePiece[][] board = new GamePiece[3][3];
 
     default GamePiece get (Coordinate coor) {
         return board[coor.x][coor.y];
@@ -13,7 +13,7 @@ public interface Game<T> {
         if(get(coor) == null) {
             board[coor.x][coor.y] = t;
         } else {
-            System.out.println("That square is already filled")
+            System.out.println("That square is already filled");
         }
     }
 
